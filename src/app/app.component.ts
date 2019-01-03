412e1662529bcc7208f6916bf59ed74f27190b2d
Akhilanand Singh <akhilanand.singh@globant.com>
2019-01-03 21:58:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {CoinService} from './coin.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div style="text-align:center">
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Price</th>
+      </tr>
+      <tr *ngFor="let coin of mycoins">
+        <td>{{coin.id}}</td>
+        <td>{{coin.name}}</td>
+        <td>{{coin.price | currency}}</td>
+
+      </tr> 
+    </table>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'basic-custom-service';
+  mycoins = [];
+  constructor(private coinservice: CoinService) {
+    this.mycoins = coinservice.getMyCoins();
+  }
 }
